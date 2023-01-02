0f923ad17c87a9c0949cbcceb792f739469244d1
Fehlpe <felpearcanjo@gmail.com>
2023-01-02 19:21:08 -0300

    ex 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
+const fibonacci = function(position) {
+    if (position < 0) return "OOPS"
+    let a = 0, b = 1;
+    let result = a;
 
+    for (let i = 0; i < position; i++) {
+        result = b;
+        [a, b] = [b, a + b];
+    }
+
+    return result;
 };
 
 // Do not edit below this line
