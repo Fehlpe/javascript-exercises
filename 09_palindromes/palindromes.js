afa985ffcc77fd2e7099568b32911f94e0952dac
Fehlpe <felpearcanjo@gmail.com>
2023-01-02 19:08:56 -0300

    ex 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let firstVersion = string.toLowerCase().replace(/[!?., ]/g, '')
+    let secondVersion = string.toLowerCase().replace(/[!?., ]/g, '').split('').reverse().join('')
+    return firstVersion == secondVersion ? true : false 
 };
 
 // Do not edit below this line
